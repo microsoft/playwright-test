41a701df14803250b2055b634e9ec9016fca5ca6
Joel Einbinder <joel.einbinder@gmail.com>
2020-04-16 22:56:34 -0700

    unit: don't require babel transformers

--- packages/unit/src/transform.ts
+++ packages/unit/src/transform.ts
@@ -3,15 +3,29 @@ import { requireResolve } from "./requireResolve";
 
 export async function transformLocalFile(filePath: string) : Promise<string>{
   const resolvedPath = await requireResolve(filePath);
+  const plugins = [];
+
+  const tsPlugin = attemptToToGetModule('@babel/plugin-transform-typescript');
+  if (tsPlugin)
+    plugins.push([tsPlugin, {isTSX: true}]);
+
+  const jsxPlugin = attemptToToGetModule('@babel/plugin-transform-react-jsx');
+  if (jsxPlugin)
+    plugins.push(jsxPlugin);
+  plugins.push('babel-plugin-third-party-imports');
   const result = await transformFileAsync(resolvedPath, {
     cwd: __dirname,
-    plugins: [
-      ['@babel/plugin-transform-typescript', {isTSX: true}],
-      ['@babel/plugin-transform-react-jsx'],
-      ['babel-plugin-third-party-imports'],
-    ]
+    plugins
   });
   if (!result || !result.code)
     throw new Error(`could not transform ${filePath}`);
   return result.code;
+
+  function attemptToToGetModule(moduleName: string) {
+    try {
+      return require(require.resolve(moduleName, {paths: [filePath, __filename]}));
+    } catch(e) {
+      return null;
+    }
+  }
 }
