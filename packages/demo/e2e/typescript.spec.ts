54bfb2d356271ea4ab8ceb6726c8ffa109643d34
Joel Einbinder <joel.einbinder@gmail.com>
2020-07-10 06:27:37 -0700

    remove unit story (#56)

--- packages/demo/e2e/typescript.spec.ts
+++
@@ -1,8 +0,0 @@
-/* ---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-it('is a basic test with the page', async ({page}) => {
-  const x : number = await page.evaluate(() => 1 + 2);
-  expect(x).toEqual(3);
-});
