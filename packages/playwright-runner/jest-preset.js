be20f1e5ba386eec89e02781d3144de1559b8b6b
Joel Einbinder <joel.einbinder@gmail.com>
2020-07-10 05:51:22 -0700

    remove unit runner from playwright-runner (#55)

--- packages/playwright-runner/jest-preset.js
+++ packages/playwright-runner/jest-preset.js
@@ -3,13 +3,5 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 module.exports = {
-  // projects: [{
-  //   displayName: {name: 'playwright', color: 'cyan'},
-  //   runner: 'jest-runner-playwright-unit',
-  //   testPathIgnorePatterns: ['/node_modules/', '/e2e/'],
-  // },{
-  // displayName: {name: 'playwright', color: 'blue'},
   runner: 'jest-runner-playwright-e2e',
-  // testMatch: ['**/__tests__/**/e2e/**/*.[jt]s?(x)', '**/e2e/**/?(*.)+(spec|test).[jt]s?(x)' ],
-  // }]
 };
