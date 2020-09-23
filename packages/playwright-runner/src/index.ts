890b1fb90e8456d731caac96492bea116cdbf0b5
Pavel <pavel.feldman@gmail.com>
2020-09-23 16:03:13 -0700

    chore: comment playwright fixtures

--- packages/playwright-runner/src/index.ts
+++ packages/playwright-runner/src/index.ts
@@ -21,35 +21,64 @@ import rimraf from 'rimraf';
 import { promisify } from 'util';
 
 import { fixtures as baseFixtures } from '@playwright/test-runner';
-import { LaunchOptions, BrowserType, Browser, BrowserContext, Page, chromium, firefox, webkit, BrowserContextOptions, devices } from 'playwright';
+import * as playwright from 'playwright';
 
 const mkdirAsync = promisify(fs.mkdir);
 const mkdtempAsync = promisify(fs.mkdtemp);
 const removeFolderAsync = promisify(rimraf);
 
+// Parameter declarations ------------------------------------------------------
+
 type PlaywrightParameters = {
+  // Browser name, one of 'chromium', 'webkit' and 'firefox', can be specified via
+  // environment BROWSER=webkit or via command line, --browserName=webkit
   browserName: string;
-  device: string | null;
+  // Run tests in a headful mode, can be specified via environment HEADFUL=1 or via
+  // command line, --headful=true. Defaults to false.
+  headful: boolean;
+  // Whether to take screenshots on failure, --screenshotOnFailure. Defaults to false.
+  screenshotOnFailure: boolean;
 };
 
+// Worker fixture declarations -------------------------------------------------
+// ... those are the same for all the test in the test file.
+
+type PlaywrightWorkerFixtures = {
+  // Browser type (Chromium / WebKit / Firefox)
+  browserType: playwright.BrowserType<playwright.Browser>;
+  // Default browserType.launch() options.
+  defaultBrowserOptions: playwright.LaunchOptions;
+  // Factory for creating a browser with given additional options.
+  browserFactory: (options?: playwright.LaunchOptions) => Promise<playwright.Browser>;
+  // Browser instance, shared for the worker.
+  browser: playwright.Browser;
+};
+
+// Test fixtures declarations, those are create for each test ------------------
+
 type PlaywrightTestFixtures = {
-  context: BrowserContext;
-  page: Page;
+  // Default browser.newContext() options.
+  defaultContextOptions: playwright.BrowserContextOptions;
+  // Factory for creating a context with given additional options.
+  contextFactory: (options?: playwright.BrowserContextOptions) => Promise<playwright.BrowserContext>;
+  // Context instance for test.
+  context: playwright.BrowserContext;
+  // Page instance for test.
+  page: playwright.Page;
+  // Temporary directory for this test's artifacts.
   tmpDir: string;
+  // Points to where the test artifacts should be written.
   outputFile: (suffix: string) => Promise<string>;
 };
 
-type PlaywrightWorkerFixtures = {
-  browserType: BrowserType<Browser>;
-  browser: Browser;
-  defaultBrowserOptions: LaunchOptions;
-  defaultContextOptions: BrowserContextOptions;
-};
+// Create the fixtures based on above ------------------------------------------
 
 export const fixtures = baseFixtures
     .declareParameters<PlaywrightParameters>()
     .declareWorkerFixtures<PlaywrightWorkerFixtures>()
     .declareTestFixtures<PlaywrightTestFixtures>();
+
+// Re-export all the test primitives for convenience.
 
 export const it = fixtures.it;
 export const fit = fixtures.fit;
@@ -63,13 +92,44 @@ export const beforeAll = fixtures.beforeAll;
 export const afterAll = fixtures.afterAll;
 export const expect = fixtures.expect;
 
+
+// Parameter and matrix definitions --------------------------------------------
+
+fixtures.defineParameter('browserName', 'Browser type name', '');
+fixtures.defineParameter('headful', 'Whether to run tests headless or headful', false);
+fixtures.defineParameter('screenshotOnFailure', 'Generate screenshot on failure', false);
+
+// If browser is not specified, we are running tests against all three browsers.
+fixtures.generateParametrizedTests(
+    'browserName',
+    process.env.BROWSER ? [process.env.BROWSER] : ['chromium', 'webkit', 'firefox']);
+
+
+// Worker fixtures definitions -------------------------------------------------
+
 fixtures.defineWorkerFixture('browserType', async ({ browserName }, test) => {
-  const browserType = ({chromium, firefox, webkit})[browserName as 'chromium' | 'firefox' | 'webkit'];
+  const browserType = playwright[browserName as 'chromium' | 'firefox' | 'webkit'];
   await test(browserType);
 });
 
-fixtures.defineParameter('browserName', 'Browser name', 'chromium');
-fixtures.defineParameter('device', 'Device name', null);
+fixtures.defineWorkerFixture('defaultBrowserOptions', async ({}, test) => {
+  await test({
+    handleSIGINT: false,
+    ...(process.env.HEADFUL ? { headless: false } : {})
+  });
+});
+
+fixtures.defineWorkerFixture('browserFactory', async ({ browserType, defaultBrowserOptions }, runTest) => {
+  const browsers: playwright.Browser[] = [];
+  async function browserFactory(options: playwright.LaunchOptions = {}) {
+    const browser = await browserType.launch({ ...defaultBrowserOptions, ...options });
+    browsers.push(browser);
+    return browser;
+  }
+  await runTest(browserFactory);
+  for (const browser of browsers)
+    await browser.close();
+});
 
 fixtures.defineWorkerFixture('browser', async ({browserType, defaultBrowserOptions}, test) => {
   const browser = await browserType.launch(defaultBrowserOptions);
@@ -77,24 +137,22 @@ fixtures.defineWorkerFixture('browser', async ({browserType, defaultBrowserOptio
   await browser.close();
 });
 
-fixtures.defineWorkerFixture('defaultContextOptions', async ({device}, test) => {
-  let contextOptions: BrowserContextOptions = {};
+// Test fixtures definitions ---------------------------------------------------
 
-  if (device && typeof device === 'string')
-    contextOptions = devices[device];
-  else if (device && typeof device === 'object')
-    contextOptions = device;
-
-  await test({
-    ...contextOptions
-  });
+fixtures.defineTestFixture('defaultContextOptions', async ({}, test) => {
+  await test({});
 });
 
-fixtures.defineWorkerFixture('defaultBrowserOptions', async ({}, test) => {
-  await test({
-    handleSIGINT: false,
-    ...(process.env.HEADFUL ? {headless: false} : {})
-  });
+fixtures.defineTestFixture('contextFactory', async ({ browser, defaultContextOptions }, runTest) => {
+  const contexts: playwright.BrowserContext[] = [];
+  async function contextFactory(options: playwright.BrowserContextOptions = {}) {
+    const context = await browser.newContext({ ...defaultContextOptions, ...options });
+    contexts.push(context);
+    return context;
+  }
+  await runTest(contextFactory);
+  for (const context of contexts)
+    await context.close();
 });
 
 fixtures.defineTestFixture('context', async ({browser, defaultContextOptions}, test) => {
@@ -104,13 +162,8 @@ fixtures.defineTestFixture('context', async ({browser, defaultContextOptions}, t
 });
 
 fixtures.defineTestFixture('page', async ({context}, runTest) => {
+  // Always create page off context so that they matched.
   await runTest(await context.newPage());
-});
-
-fixtures.defineTestFixture('tmpDir', async ({ }, test) => {
-  const tmpDir = await mkdtempAsync(path.join(os.tmpdir(), 'playwright-test-'));
-  await test(tmpDir);
-  await removeFolderAsync(tmpDir).catch(e => { });
 });
 
 fixtures.defineTestFixture('outputFile', async ({ testInfo }, runTest) => {
@@ -126,4 +179,10 @@ fixtures.defineTestFixture('outputFile', async ({ testInfo }, runTest) => {
     return assetPath;
   };
   await runTest(outputFile);
+});
+
+fixtures.defineTestFixture('tmpDir', async ({ }, test) => {
+  const tmpDir = await mkdtempAsync(path.join(os.tmpdir(), 'playwright-test-'));
+  await test(tmpDir);
+  await removeFolderAsync(tmpDir).catch(e => { });
 });
