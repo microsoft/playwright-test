6d93c18ef8c9aafa6879c41d630fa2f40ab3e61d
Pavel <pavel.feldman@gmail.com>
2020-09-23 17:11:40 -0700

    chore: bump version to 0.3.24

--- scripts/packages.js
+++ scripts/packages.js
@@ -17,9 +17,10 @@
 const { GlobSync } = require('glob');
 const path = require('path');
 const root = path.join(__dirname, '..');
-const packages = GlobSync('./[packages,examples]/*/package.json', {cwd: root})
+const packages = GlobSync('./{packages,examples}/*/package.json', { cwd: root })
     .found
     .map(file => {
+      console.log(file);
       const fileName = path.join(root, file);
       const json = require(fileName);
       return {fileName, json};
