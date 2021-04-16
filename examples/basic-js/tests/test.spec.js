e1811631b61a6072ef0dec308004a830ea6a3297
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2021-04-16 12:50:38 +0900

    fix(examples/basic-js): Bump @playwright/test from 0.162.0 to 0.1101.0

--- examples/basic-js/tests/test.spec.js
+++ examples/basic-js/tests/test.spec.js
@@ -14,9 +14,9 @@
  * limitations under the License.
  */
 
-const { it, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-it('is a basic test with the page', async ({ page }) => {
+test('is a basic test with the page', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   expect(await page.innerText('.navbar__title')).toBe('Playwright');
 });
